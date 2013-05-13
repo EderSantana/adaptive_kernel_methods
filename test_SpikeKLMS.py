6aedb966c565947ff4798814fa702a991f8c1cf9
edersantana <edersantana@macbookpro.com>
2013-05-13 18:53:33 -0300

    added test_SpikeKLMS.py

---
+++ test_SpikeKLMS.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 12:14:44 2013
+
+@author: eder
+"""
+from numpy import squeeze, newaxis, linspace
+
+import spike_klms
+
+reload(spike_klms)
+from spike_klms import SpikeKLMS
+from scipy.io import loadmat
+sts = loadmat("sts.mat").get("sts")
+X = range(len(sts))
+for i in xrange(len(X)):
+    X[i] = squeeze(sts[i,0])
+    if not(X[i].shape):
+        X[i] = X[i][newaxis]
+d   = squeeze(loadmat("targets.mat").get("targets"))
+t   = linspace(1,500,500)
+sklms = SpikeKLMS(learning_rate=.09, ksize=.005, growing_criterion="novelty",\
+growing_param = [.1, .01])
+sklms.fit_transform(X, d)
